426e461eca2de0172768ba5c2b2e792d9171d7e0
Trushna Rabadia <rabadiatrushna@gmail.com>
2022-02-16 17:16:29 +0000

    Setting up a Testing Environment

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Connect to psql
 Create the database using the psql command CREATE DATABASE bookmark_manager;
 Connect to the database using the pqsl command \c bookmark_manager;
 Run the query we have saved in the file 01_create_bookmarks_table.sql
+
+
+IN ORDER TO SET UP BEFOREHAND:
+
+Connect to psql
+Create the database using the psql command CREATE DATABASE bookmark_manager_test;
+Connect to the database using the pqsl command \c bookmark_manager;
+Run the query we have saved in the file 01_create_bookmarks_table.sql
--- spec/features/view_bookmarks_list_spec.rb
+++ spec/features/view_bookmarks_list_spec.rb
@@ -1,5 +1,13 @@
+require 'pg'
+
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    
+    connection.exec("INSERT INTO bookmarks VALUES (1, 'http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
     visit('/bookmarks')
 
     expect(page).to have_content "http://www.makersacademy.com"
